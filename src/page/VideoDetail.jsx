4db5fd39c9d9d9bcc2ffe6853b5b64661e5134f4
A-ri-SIM <149152400+A-ri-SIM@users.noreply.github.com>
2024-07-26 23:14:54 +0900

    https 수정

--- src/page/VideoDetail.jsx
+++ src/page/VideoDetail.jsx
@@ -54,7 +54,7 @@ export default function VideoDetail() {
               type="text/html"
               width="100%"
               height="700"
-              src={`http://www.youtube.com/embed/${video.id}`}
+              src={`https://www.youtube.com/embed/${video.id}`}
               frameBorder="0"
               className="rounded-lg"
               title={title}
