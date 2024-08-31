05382743a0afff60efcf8351695de91fc37c545b
A-ri-SIM <149152400+A-ri-SIM@users.noreply.github.com>
2024-08-31 18:14:08 +0900

    searchbar 수정

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -40,7 +40,7 @@ export default function SearchHeader({ isClick, setIsClick }) {
       >
         <input
           id="searchBar"
-          className="w-5/12 pl-4 border border-zinc-600 text-base text-black rounded-s-full max-[640px]:searchbar"
+          className="w-5/12 pl-4 border border-zinc-600 text-base text-black rounded-s-full max-[640px]:hidden max-[640px]:searchbar"
           type="text"
           placeholder="Search..."
           value={text}
